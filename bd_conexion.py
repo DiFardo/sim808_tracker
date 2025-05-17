a5cfe2f883895da0ae21f85ca9f710d62a8944f6
Eduardo Diaz <eduardofabioelmaspro@gmail.com>
2025-05-16 23:36:56 -0500

    cambio bd

--- bd_conexion.py
+++ bd_conexion.py
@@ -3,9 +3,9 @@ import psycopg2
 
 def obtener_conexion():
     return psycopg2.connect(
-        host=os.environ.get('POSTGRES_HOST'),
-        port=os.environ.get('POSTGRES_PORT'),
-        user=os.environ.get('POSTGRES_USER'),
-        password=os.environ.get('POSTGRES_PASSWORD'),
-        database=os.environ.get('POSTGRES_DB')
+        host=os.getenv('POSTGRES_HOST'),
+        port=os.getenv('POSTGRES_PORT'),
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        database=os.getenv('POSTGRES_DB')
     )
